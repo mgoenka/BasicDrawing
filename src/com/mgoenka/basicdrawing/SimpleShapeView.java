768f32d2e5136a6a27908dfcb2d733e4504d9731
Mohit Goenka <mgoenka@yahoo-inc.com>
2014-03-25 17:08:07 -0700

    Added custom view

---
+++ src/com/mgoenka/basicdrawing/SimpleShapeView.java
@@ -0,0 +1,11 @@
+package com.mgoenka.basicdrawing;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SimpleShapeView extends View {
+    public SimpleShapeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
