dc39bf7a619b471fb4fa64e2b6aee94c9d835828
Mohit Goenka <mgoenka@yahoo-inc.com>
2014-03-25 17:22:03 -0700

    Created simple circles

--- src/com/mgoenka/basicdrawing/SimpleShapeView.java
+++ src/com/mgoenka/basicdrawing/SimpleShapeView.java
@@ -1,11 +1,44 @@
 package com.mgoenka.basicdrawing;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class SimpleShapeView extends View {
+	// Setup initial color
+	private final int paintColor = Color.BLACK;
+	
+	// Defines Paint and Canvas
+	private Paint drawPaint;
+	
     public SimpleShapeView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+    
+    @Override
+    protected void onDraw(Canvas canvas) {
+    	super.onDraw(canvas);
+    	canvas.drawCircle(50, 50, 20, drawPaint);
+        drawPaint.setColor(Color.GREEN);
+        canvas.drawCircle(50, 150, 20, drawPaint);
+        drawPaint.setColor(Color.BLUE);
+        canvas.drawCircle(50, 250, 20, drawPaint);
+    }
+    
+    // Setup paint with color and stroke styles
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 }
